bd862233e6b7468cebcb03a1c9e5c12629f7ead4
AISHWARY <aish9045@gmail.com>
2025-06-18 13:55:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Header from '../components/header/Header';
 import Home from '../components/home/Home';
 import About from '../components/about/About';
 import Skills from '../components/skills/Skills';
-import Experience from '../components/experience/Experience';
+
 import Qualification from '../components/qualification/Qualification';
 import Work from '../components/work/Work';
-import Testimonials from '../components/testimonials/Testimonials';
+
 import Contact from '../components/contact/Contact';
 import Footer from '../components/footer/Footer';
 import ScrollUp from '../components/scrollup/ScrollUp';
